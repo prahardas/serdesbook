212fb70ee58dbcc75a6013c1c7402737f478056a
Prasad Hardas <prasadhardas@Prasads-MBP.attlocal.net>
2025-06-18 09:45:34 -0700

    Deploy website - based on fc845c00741f53e34f4127984a92f3ae0e2fa317

---
+++ assets/js/acecf23e.6736715a.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkserdesbook=self.webpackChunkserdesbook||[]).push([[1903],{1912:s=>{s.exports=JSON.parse('{"blogBasePath":"/blog","blogTitle":"Blog","authorsListPath":"/blog/authors"}')}}]);
